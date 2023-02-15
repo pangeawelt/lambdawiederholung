4659ddb47fa35943dce59e3f9ad90fefd6bbad3b
ganiebuer <72691811+pangeawelt@users.noreply.github.com>
2023-02-15 01:11:18 +0100

    Add files via upload

--- UtilsforLambda.java
+++ UtilsforLambda.java
@@ -1,4 +1,4 @@
-package java_core.day36lambda;
+package java_core.day37lambda;
 //wieder und wieder spezielle Methoden erforderlich
 // wie Hammer braucht man
 
@@ -10,6 +10,20 @@ public class UtilsforLambda {
     public static boolean isGeradeZahl(String s){//ist es gerade oder?
         return s.length()%2==0;
     }
+
+    public static boolean isGeradeZahl(int s){//ist es gerade oder?
+        return s%2==0;
+    }
+ public static int getSumvonZiffer(int x){
+
+        int sum = 0;
+        while (x!=0){
+            sum += x%10; // zuerste Ziffer von letzte nimmt danach addiert sum danach erzuegt neu Container
+            x=x/10; //decrement
+        }
+        return sum;
+ }
+
 
     public static String getNimmtChar(String s){
         return s.substring(s.length()-1);
